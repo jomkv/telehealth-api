b277dbfeb7b689eec051bb394efdf96b619314c2
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-05-25 18:42:57 +0800

    feat: create user endpoint init

---
+++ src/api/user/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { hash } from 'bcryptjs';
+import { UserRole } from 'generated/prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const passwordHash = await hash(createUserDto.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        username: createUserDto.username,
+        email: createUserDto.email,
+        password: passwordHash,
+        role: UserRole.USER,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        role: true,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
