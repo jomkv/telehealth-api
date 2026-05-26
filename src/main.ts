0604a2d33e52d6fe875c1f7abed2ed7f82b2f0ad
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-05-26 21:01:32 +0800

    fix: use cookieParser, enableCors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 import { AllExceptionFilter } from './shared/filters/exception.filter';
+import * as cookieParser from 'cookie-parser';
+import { ENV_VARS } from './shared/env-variables';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ENV_VARS.clientUrl(),
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionFilter());
