d1415820f16b1fc3893a109c8d69a04896a0e974
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-05-30 20:17:35 +0800

    chore: explicitly declare DB pool

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,19 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { ENV_VARS } from 'src/shared/env-variables';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: ENV_VARS.dbUrl(),
+      max: 11, // leave headroom for 22 limit
+      idleTimeoutMillis: 30000, // release idle connections after 30s
+      connectionTimeoutMillis: 5000,
     });
+
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 
